484290a331c1c69a3b72644caa7f17017210e804
WissemMliki <54664322+WissemMliki@users.noreply.github.com>
2025-05-15 13:46:20 +0200

    Update Controlable.java

--- src/main/java/org/roboticRover/rover/Controlable.java
+++ src/main/java/org/roboticRover/rover/Controlable.java
@@ -4,8 +4,6 @@ import org.roboticRover.exception.InvalidOrderException;
 
 /**
  * Interface defining basic rover movements: turn left, turn right, and move forward.
- * Includes a default apply() method to execute a movement based on a single character command ('L', 'R', 'M').
- * Throws InvalidOrderException for unsupported commands.
  */
 
 public interface Controlable {
